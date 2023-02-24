1461abefc69b1517aea7622200962e88a4379cdb
Sergey Grankin <sgrankin@gmail.com>
2023-02-24 16:22:29 -0500

    Track terraform in tools.go

--- main.go
+++ main.go
@@ -12,11 +12,10 @@ import (
 // Generate graphql client.
 //go:generate go run github.com/Khan/genqlient
 
-// Format the terraform.
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
+// Format the terraform examples.
+//go:generate go run github.com/hashicorp/terraform fmt -recursive ./examples/
 
-// Run the docs generation tool, check its repository for more information on how it works and how docs can be customized.
+// Run the docs generation tool. Check its repository for more information on how it works and how docs can be customized.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 var (
--- tools.go
+++ tools.go
@@ -4,5 +4,6 @@ package tools
 
 import (
 	_ "github.com/Khan/genqlient"
+	_ "github.com/hashicorp/terraform"
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
